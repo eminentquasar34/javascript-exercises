c1b6d46b3c3190ac3a09a60f03e0a2abf2ee5911
Daniel Zhang <72416560+eminentquasar34@users.noreply.github.com>
2024-03-31 20:06:49 -0700

    finished prob 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    final = "";
+    for (let i = word.length; i > 0; i--) {
+        final += word[i - 1];
+    }
+
+    return final;
 };
 
 // Do not edit below this line
