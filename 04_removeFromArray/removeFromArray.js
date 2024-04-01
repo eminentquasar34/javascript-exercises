ce9d54a61d43e1562fc68d5ec636449e012590cf
Daniel Zhang <72416560+eminentquasar34@users.noreply.github.com>
2024-03-31 23:54:36 -0700

    finished project 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const final = [];
+    let deleted = false;
+    for (let l = 0; l <= array.length - 1; l++) {
+        deleted = false;
+        for (let i = 0; i <= args.length - 1; i ++) {
+            if (array[l] === args[i]) {
+                deleted = true;
+            }
+        }
 
+        if (!deleted) {
+            final.push(array[l]);
+        }
+    }
+    return final;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
