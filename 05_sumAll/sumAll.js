85abf7bad3f795f49d3fc3362d36d002544d1f7b
Daniel Zhang <72416560+eminentquasar34@users.noreply.github.com>
2024-08-08 12:16:10 -0700

    finished problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (first < 0 || second < 0 || !Number.isInteger(first) || !Number.isInteger(second)) {
+        return "ERROR";
+    }
+    let output = 0;
+    let [min, max] = [0, 0]
+    if (first < second) {
+        [min, max] = [first, second];
+    } else if (second < first) {
+        [min, max] = [second, first];
+    } else {
+        return first;
+    }
+
+    for (let i = min; i <= max; i ++) {
+        output += i;
+    }
+    
+    return output;
 
 };
 
