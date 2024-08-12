673807ebaa8fd2ed17313ccc9d030bb5d3a394d0
Daniel Zhang <72416560+eminentquasar34@users.noreply.github.com>
2024-08-12 16:04:29 -0700

    finished problem 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let output = (temp - 32) * 5 / 9;
+  output = Math.round(output * 10)/10;
+  return output;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let output = (temp * 9/5) + 32;
+  output = Math.round(output * 10) / 10;
+  return output;
 };
 
 // Do not edit below this line
